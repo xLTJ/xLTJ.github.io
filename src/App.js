0a11fb09ed8e8b1871606c47c71a321b7e640bb6
xLTJ <lxcy.3@proton.me>
2024-02-10 20:16:45 +0100

    waow

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Need to set this up later lol</h1>
+            </header>
+            <main className={"projects"}>
+                <div className={"project"}>
+                    <a className={"site-link"} href={"http://lucilorate.me/Better-Definitions/"}>
+                        <img src={"https://media.discordapp.net/attachments/1053619418371985452/1205921850706890762/1707584572431.png?ex=65da213e&is=65c7ac3e&hm=043a20126afd2bc3788de0ebcd73547c57a42425d632d5f6c837a03a6e829c49&=&format=webp&quality=lossless&width=1238&height=669"}/>
+                        <h3>Better Definitions</h3>
+                    </a>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default App;
